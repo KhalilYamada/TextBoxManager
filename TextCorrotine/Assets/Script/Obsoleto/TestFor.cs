98415f2d40022b70b4d1a551895d3163278382a4
Khalil Yamada Gangeme <khalil.yamada@hotmail.com>
2020-05-25 22:26:48 -0300

    First commit
    Colocando o projeto inteiro que já estava funcionando ontem

---
+++ TextCorrotine/Assets/Script/Obsoleto/TestFor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFor : MonoBehaviour
+{
+    bool ativou = true;
+    bool textBoxLigada = false;
+    public GameObject textBox;
+
+
+    private void Start()
+    {
+        textBox.SetActive(false);
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && ativou == true)
+        {
+            ativou = false;
+            StartCoroutine(Teste());
+        }
+    }
+
+
+    IEnumerator Teste()
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            Debug.Log("reescreve texto");
+            if(textBoxLigada == false)
+            {
+                textBox.SetActive(true);
+                textBoxLigada = true;
+            }
+            yield return new WaitForSeconds(1);
+        }
+        Debug.Log("acabou");
+        textBox.SetActive(false);
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ TextCorrotine/Assets/Script/TextManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour
+{
+    public GameObject textBox;
+
+    bool ativou = true;
+    bool textBoxLigada = false;
+
+    [HideInInspector]
+    public int currentLine = 0;
+    [HideInInspector]
+    public int endAtLine;
+
+    public Text theText;   
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    private void Start()
+    {
+        textBox.SetActive(false);
+        AssimilaTexto();
+        endAtLine = textLines.Length;
+    }
+
+
+    void AssimilaTexto()
+    {
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && ativou == true)
+        {
+            Debug.Log("asd");
+            ativou = false;
+            StartCoroutine(Teste());
+        }
+    }
+
+
+    IEnumerator Teste()
+    {
+        for (int i = 0; i < endAtLine; i++)
+        {
+            theText.text = textLines[currentLine];
+            if (textBoxLigada == false)
+            {
+                textBox.SetActive(true);
+                textBoxLigada = true;
+            }
+            yield return new WaitForSeconds(3);
+            currentLine++;
+        }
+        textBox.SetActive(false);
+        Destroy(gameObject);
+    }
+}
